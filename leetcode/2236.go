742db3c7b18197b50e5c1bc9265af46967a2b289
Kevin Liao <q85292542000@gmail.com>
2022-11-13 05:08:38 +0800

    feat: add leetcode 2236

---
+++ leetcode/2236.go
@@ -0,0 +1,11 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func checkTree(root *TreeNode) bool {
+	return (root.Left.Val + root.Right.Val) == root.Val
+} 
