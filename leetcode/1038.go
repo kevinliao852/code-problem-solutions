c4b4ae07686475d256cecda3453d6f231dd90cd1
Kevin Liao <q85292542000@gmail.com>
2022-11-13 06:35:39 +0800

    feat: add leetcode 1038

---
+++ leetcode/1038.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func bstToGst(root *TreeNode) *TreeNode {
+	var sum int
+	var traverseAndPlusSum func(*TreeNode)
+
+	traverseAndPlusSum = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		traverseAndPlusSum(root.Right)
+		sum += root.Val
+		root.Val = sum
+		traverseAndPlusSum(root.Left)
+
+	}
+
+	traverseAndPlusSum(root)
+
+	return root
+}
+
+// same as leetcode 538
