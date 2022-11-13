bc7c1a854b3d8b56b2e1bdc220844fc7f40e8962
Kevin Liao <q85292542000@gmail.com>
2022-11-13 15:18:48 +0800

    feat: add leetcode 724

---
+++ leetcode/724.go
@@ -0,0 +1,21 @@
+func pivotIndex(nums []int) int {
+	// 1 <= nums.length <= 104
+	// -1000 <= nums[i] <= 1000
+	var sum int
+	var prefixSum int
+
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if prefixSum*2+nums[i] == sum {
+			return i
+		}
+		prefixSum += nums[i]
+	}
+
+	return -1
+}
+
+// the same as leetcode 1991
