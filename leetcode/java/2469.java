e8f5f89f4a90e98c18c5707588beea891f4372b6
Kevin Liao <q85292542000@gmail.com>
2022-12-11 10:12:27 +0800

    feat: add leetcode 2469.java

---
+++ leetcode/java/2469.java
@@ -0,0 +1,10 @@
+class Solution {
+  public double[] convertTemperature(double celsius) {
+    double[] res = new double[2];
+
+    res[0] = celsius + 273.15;
+    res[1] = celsius * 1.80 + 32.00;
+
+    return res;
+  }
+}
