b2f04b687001be719c31001ed055807fb5d796d2
Kevin Liao <q85292542000@gmail.com>
2022-12-17 03:04:26 +0800

    style: format 232.go

--- leetcode/go/232.go
+++ leetcode/go/232.go
@@ -1,68 +1,61 @@
 type MyQueue struct {
-    input []int
-    output []int
+	input  []int
+	output []int
 }
-
 
 func Constructor() MyQueue {
-    return MyQueue{}
+	return MyQueue{}
 }
 
-
-func (this *MyQueue) Push(x int)  {
-    this.input = append(this.input, x)
+func (this *MyQueue) Push(x int) {
+	this.input = append(this.input, x)
 }
-
 
 func (this *MyQueue) Pop() int {
-    var res int 
-    if len(this.output) == 0 {
-      for len(this.input) > 0 {
-          v := this.input[len(this.input) - 1] 
-          this.input = this.input[:len(this.input) - 1]
-          this.output = append(this.output, v)
-      } 
-    } 
+	var res int
+	if len(this.output) == 0 {
+		for len(this.input) > 0 {
+			v := this.input[len(this.input)-1]
+			this.input = this.input[:len(this.input)-1]
+			this.output = append(this.output, v)
+		}
+	}
 
-    if len(this.output) == 0 {
-        return res
-    }
+	if len(this.output) == 0 {
+		return res
+	}
 
-    res = this.output[len(this.output) - 1]
-    this.output = this.output[:len(this.output) - 1]
+	res = this.output[len(this.output)-1]
+	this.output = this.output[:len(this.output)-1]
 
-    return res
+	return res
 }
-
 
 func (this *MyQueue) Peek() int {
-    var res int
+	var res int
 
-    if len(this.output) == 0 {
-      for len(this.input) > 0 {
-          v := this.input[len(this.input) - 1]
-          this.input = this.input[:len(this.input) - 1]
-          this.output = append(this.output, v)
-      } 
-    }
+	if len(this.output) == 0 {
+		for len(this.input) > 0 {
+			v := this.input[len(this.input)-1]
+			this.input = this.input[:len(this.input)-1]
+			this.output = append(this.output, v)
+		}
+	}
 
-   
-    res = this.output[len(this.output) - 1]
+	res = this.output[len(this.output)-1]
 
-    return res    
+	return res
 }
-
 
 func (this *MyQueue) Empty() bool {
-    var res bool
+	var res bool
 
-    if len(this.input) == 0 && len(this.output) == 0 {
-        res = true
-    }
+	if len(this.input) == 0 && len(this.output) == 0 {
+		res = true
+	}
 
-    return res
+	return res
 }
-
 
 /**
  * Your MyQueue object will be instantiated and called as such:
